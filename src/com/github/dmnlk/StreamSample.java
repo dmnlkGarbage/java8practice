29d509952e8a94996eb8cac6e916b616583b7f5f
dmnlk <seikima2demon@gmail.com>
2015-03-15 23:50:33 +0900

    java

--- src/com/github/dmnlk/StreamSample.java
+++ src/com/github/dmnlk/StreamSample.java
@@ -1,10 +1,16 @@
 package com.github.dmnlk;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by masaki on 2015/03/15.
  */
 public class StreamSample {
     public static void main(String[] args) {
-        System.out.print("a");
+        List<Integer> list = new ArrayList<Integer>();
+        for (Integer a : list) {
+    
+        }
     }
 }
