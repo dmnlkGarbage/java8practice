917df234af59b3e5c1bc08bf68e08f40cf791119
dmnlk <seikima2demon@gmail.com>
2015-03-15 23:47:17 +0900

    add sample

---
+++ src/com/github/dmnlk/StreamSample.java
@@ -0,0 +1,10 @@
+package com.github.dmnlk;
+
+/**
+ * Created by masaki on 2015/03/15.
+ */
+public class StreamSample {
+    public static void main(String[] args) {
+        System.out.print("a");
+    }
+}
