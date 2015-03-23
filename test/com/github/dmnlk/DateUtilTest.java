b1f7da32c956f32e8e82c2746073497a9df50bd5
dmnlk <seikima2demon@gmail.com>
2015-03-24 00:22:10 +0900

    test timestamp

--- test/com/github/dmnlk/DateUtilTest.java
+++ test/com/github/dmnlk/DateUtilTest.java
@@ -2,13 +2,15 @@ package com.github.dmnlk;
 
 import org.junit.Test;
 
+import java.sql.Timestamp;
+
 import static org.junit.Assert.*;
 
 public class DateUtilTest {
 
     @Test
     public void testGetNowTimestamp() throws Exception {
-        System.out.println(DateUtil.getNowTimestamp());
-        assertTrue(true);
+        Timestamp nowTimestamp = DateUtil.getNowTimestamp();
+        assertTrue(nowTimestamp instanceof  Timestamp);
     }
 }
