168a80488f6340e9df39541046a2b62b2b6ed3b3
dmnlk <seikima2demon@gmail.com>
2015-04-06 00:39:14 +0900

    optional sample

--- src/com/github/dmnlk/OptionalSample.java
+++ src/com/github/dmnlk/OptionalSample.java
@@ -1,11 +1,27 @@
 package com.github.dmnlk;
 
+import java.util.Optional;
+
 /**
  * Created by dmnlk on 2015/04/06.
  */
 public class OptionalSample {
 
     public static void main(String[] args) {
-        
+
+        //java7
+        String hoge = getHoge();
+        if (hoge != null) {
+            System.out.println(hoge);
+        }
+
+        //java8
+        Optional<String> optionalHoge = Optional.ofNullable(getHoge());
+        optionalHoge.isPresent(s -> System.out.println(s));
+    }
+
+    // なんかnull返すかもしれないメソッド
+    public static String getHoge() {
+        return  null;
     }
 }
