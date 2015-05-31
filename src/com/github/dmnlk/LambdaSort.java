0184bfadb62d976a0b453ddc7f26c22b0734e56d
dmnlk <seikima2demon@gmail.com>
2015-05-31 12:51:23 +0900

    arrayで処理しないとだめっぽい？

--- src/com/github/dmnlk/LambdaSort.java
+++ src/com/github/dmnlk/LambdaSort.java
@@ -10,8 +10,10 @@ public class LambdaSort {
     public static void main(String[] args) {
         List<String> ts = Arrays.asList("a", "bb", "ccc", "d");
 
+        String[] strings = ts.toArray(new String[ts.size()]);
 
+        Arrays.sort(strings, (s, t) -> Integer.compare(s.length(), t.length()));
 
-        Arrays.sort(ts, (String s, String t) -> Integer.compare(s.length(), t.length()));
+        Arrays.asList(strings).forEach(System.out::println);
     }
 }
