2b9b97312aa5b277a6b22ae667466a2c777d72fb
dmnlk <seikima2demon@gmail.com>
2015-06-05 23:28:55 +0900

    use Consumer Function

--- src/com/github/dmnlk/Consumer.java
+++ src/com/github/dmnlk/Consumer.java
@@ -1,10 +1,14 @@
 package com.github.dmnlk;
 
+import java.util.function.Function;
+
 /**
  * @author dmnlk
  */
 public class Consumer {
     public static void main(String[] args) {
-        
+        //T型引数を受け取りR型を返す
+        Function<String, Integer> function = str -> Integer.parseInt(str);
+        System.out.println(function.apply("2"));
     }
 }
