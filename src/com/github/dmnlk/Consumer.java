9f18f32da66e413eb9b5f43a28be9fcd251f6ede
dmnlk <seikima2demon@gmail.com>
2015-06-06 23:39:03 +0900

    compose

--- src/com/github/dmnlk/Consumer.java
+++ src/com/github/dmnlk/Consumer.java
@@ -10,5 +10,15 @@ public class Consumer {
         //T型引数を受け取りR型を返す
         Function<String, Integer> function = str -> Integer.parseInt(str);
         System.out.println(function.apply("2"));
+
+
+        //compose
+        // Function作ってそれにcomposeで追加して新しいFunctionを作る。それにapplyすると両方が適用される
+        Function<String, String > wrapDoubleQuote = str -> "\"" + str + "\"";
+        Function<String, String > wrapSingleQuote = str -> "'" + str + "'";
+
+        Function<String, String > compose = wrapDoubleQuote.compose(wrapSingleQuote);
+        String result = compose.apply("hoge");
+        System.out.println(result);
     }
 }
